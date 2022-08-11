45a6e84040459bf65c346994eac55ffa3584b9ac
Léo GUISLAIN <guislainleo@gmail.com>
2022-08-11 15:29:30 +0200

    remove comments

--- connect.php
+++ connect.php
@@ -16,10 +16,7 @@ function login(){
     $findUser->bindParam(':login_user', $_POST['username'], PDO::PARAM_STR);
     $findUser->execute();
     $user = $findUser->fetch();
- 
-    //$user && password_verify($_POST['password'], $user['password_user'])
-    //le mot de passe est alex
-    //$user && password_verify(alex, $2y$10$6BcBM4oinhbyHIL09w.j/eIFwYkCc499VDIZIy7LJ1PRU4GO2ynQS])
+
     if ($user && password_verify($_POST['password'], $user['password'])) {
         $_SESSION['nom_user'] = $user['name'];
         header('Location: ./index.php');  
